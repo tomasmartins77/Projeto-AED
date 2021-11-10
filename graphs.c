681cab253f5f9890a2e2d140b7e1950264b203ee
Rita Mendes <rcordeiromendes@gmail.com>
2021-11-10 01:12:26 +0000

    comments graphs e utility2

--- graphs.c
+++ graphs.c
@@ -8,11 +8,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+/** \brief cria e preenche o grafo cujos vertices correspondem ao numero de salas
+ *
+ * \param salas numero total de salas
+ * \param lab lab_t
+ * \param tab int** labirinto completo
+ * \return Graph grafo
+ *
+ */
 Graph *readGraph(int salas, lab_t lab, int **tab)
 {
     int x = 0, y = 0, BAI = 0, CIM = 0, ESQ = 0, DIR = 0;
 
-    /*allocate graph*/
+    /*alocar memoria para o grafo*/
     Graph *graph = (Graph *)malloc(sizeof(Graph));
     if (graph == NULL)
         exit(1);
@@ -27,6 +35,7 @@ Graph *readGraph(int salas, lab_t lab, int **tab)
     {
         for (y = 0; y < lab.colunas; y++)
         {
+            /*se nao for quebravel*/
             if (quebravel(tab, lab, x, y) == 0)
                 continue;
 
@@ -35,6 +44,7 @@ Graph *readGraph(int salas, lab_t lab, int **tab)
             CIM = x - 1;
             BAI = x + 1;
 
+            /*ver quais os vizinhos dessa parede */
             vizinhos(tab, lab, x, y, &ESQ, &DIR, &CIM, &BAI);
 
             put_vertice(graph, tab, ESQ, DIR, x, y);
@@ -44,6 +54,12 @@ Graph *readGraph(int salas, lab_t lab, int **tab)
     return graph;
 }
 
+/** \brief liberta a memoria alocada para o grafo
+ *
+ * \param graph Graph*
+ * \return void
+ *
+ */
 void freeGraph(Graph *graph)
 {
     int i = 0;
@@ -58,14 +74,23 @@ void freeGraph(Graph *graph)
     return;
 }
 
+/** \brief verifica se o vertice ja se encontra na lista
+ *
+ * \param first Lista* primeiro elemento da lista
+ * \param value int
+ * \return int 1 se estiver na lista, caso contrario 0
+ *
+ */
 int verifica_vertice(Lista *first, int value)
 {
     Lista *aux = first;
     Edge *edge;
 
+    /*percorre a lista*/
     while (aux != NULL)
     {
         edge = getItemLista(aux);
+        /*se o vertice ja estiver na lista*/
         if (edge->V == value)
             return 1;
 
@@ -74,11 +99,22 @@ int verifica_vertice(Lista *first, int value)
     return 0;
 }
 
+/** \brief troca o valor de uma aresta se o seu peso for inferior 
+ *
+ * \param first Lista* primeiro elemento da lista
+ * \param aresta int 
+ * \param value int
+ * \param x int posicao x
+ * \param y int posicao x
+ * \return void
+ *
+ */
 void troca_value(Lista *first, int aresta, int value, int x, int y)
 {
     Lista *aux = first;
     Edge *edge;
 
+    /*percorrer a lista*/
     while (aux != NULL)
     {
         edge = (Edge *)getItemLista(aux);
@@ -86,7 +122,7 @@ void troca_value(Lista *first, int aresta, int value, int x, int y)
         {
             if (edge->W > value)
             {
-                edge->W = value;
+                edge->W = value; /*trocar para a aresta de menor peso*/
                 edge->x = x;
                 edge->y = y;
                 return;
@@ -97,17 +133,31 @@ void troca_value(Lista *first, int aresta, int value, int x, int y)
     return;
 }
 
+/** \brief coloca os vertices nas listas de adjacencias respetivas
+ *
+ * \param graph Graph* grafo
+ * \param tab int** labirinto completo
+ * \param a int
+ * \param b int
+ * \param x int posicao x
+ * \param y int posicao x
+ * \return void
+ *
+ */
 void put_vertice(Graph *graph, int **tab, int a, int b, int x, int y)
 {
     Edge *edge;
     if (a != 0 && b != 0)
     {
         if (verifica_vertice(graph->adj[-a - 3], -b - 3) == 0 && verifica_vertice(graph->adj[-b - 3], -a - 3) == 0)
+        /*inserir vertice na lista*/
         {
+            /* Allocates new v1 edge */
             edge = (Edge *)malloc(sizeof(Edge));
             if (edge == NULL)
                 exit(3);
 
+            /*guarda v2 na lista de adjacencias de v1*/
             edge->V = -b - 3;
             edge->W = tab[x][y];
             edge->x = x;
@@ -119,12 +169,14 @@ void put_vertice(Graph *graph, int **tab, int a, int b, int x, int y)
             if (edge == NULL)
                 exit(3);
 
+            /*guarda v1 na lista de adjacencias de v2*/
             edge->V = -a - 3;
             edge->W = tab[x][y];
             edge->x = x;
             edge->y = y;
             graph->adj[-b - 3] = insertSortedLista(graph->adj[-b - 3], edge, comparisonItemFnt);
         }
+        /*se o vertice ja estiver na lista*/
         else
         {
             troca_value(graph->adj[-a - 3], -b - 3, tab[x][y], x, y);
